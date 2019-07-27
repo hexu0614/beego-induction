bd950d84326fec1f6320be0a83dca4f58a41bedb
hexu <xu_he@shannonai.com>
2019-07-27 17:42:54 +0800

    add/how to return json

--- example/src/hello_world/controllers/hwcontroller.go
+++ example/src/hello_world/controllers/hwcontroller.go
@@ -6,7 +6,19 @@ type HWController struct {
 	beego.Controller
 }
 
-func (hw *HWController) Get() {      // get请求
-	hw.TplName = "hello world.html"  // 关联静态文件
+type JSONStruct struct {
+	Code int
+	Msg  string
+	Data string
 }
 
+//func (hw *HWController) Get() {      // get请求
+//	hw.TplName = "hello world.html"  // 关联静态文件
+//}
+
+func (hw *HWController) Get() {      // get请求
+	json := &JSONStruct{0, "Success", "hello json"}
+	hw.Data["json"] = json
+	hw.ServeJSON()
+	return
+}
